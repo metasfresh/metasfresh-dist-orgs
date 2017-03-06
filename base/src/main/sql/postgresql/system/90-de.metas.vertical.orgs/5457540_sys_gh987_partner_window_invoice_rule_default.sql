c335380ec8deed54a1447a633a802fcebedc27bd
metasnw <norbert.wessel@metasfresh.com>
2017-03-06 16:27:57 +0100

    #987 changing default invoice rule
    removing default values on bpartner and adding it to invoice candidate
    invoice rule override

--- base/src/main/sql/postgresql/system/90-de.metas.vertical.orgs/5457540_sys_gh987_partner_window_invoice_rule_default.sql
+++ base/src/main/sql/postgresql/system/90-de.metas.vertical.orgs/5457540_sys_gh987_partner_window_invoice_rule_default.sql
@@ -1,13 +1,15 @@
--- 04.03.2017 13:22
--- URL zum Konzept
-UPDATE AD_Column SET DefaultValue='I',Updated=TO_TIMESTAMP('2017-03-04 13:22:28','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=4429
-;
--- 04.03.2017 20:46
--- URL zum Konzept
-INSERT INTO t_alter_column values('c_bpartner','InvoiceRule','CHAR(1)',null,'I')
-;
-
 -- 04.03.2017 20:42
 -- URL zum Konzept
 UPDATE AD_Field SET DefaultValue='2000837',Updated=TO_TIMESTAMP('2017-03-04 20:42:44','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=545322
 ;
+
+-- 06.03.2017 16:26
+-- URL zum Konzept
+UPDATE AD_Column SET DefaultValue='I',Updated=TO_TIMESTAMP('2017-03-06 16:26:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=545847
+;
+
+-- 06.03.2017 16:26
+-- URL zum Konzept
+INSERT INTO t_alter_column values('c_invoice_candidate','InvoiceRule_Override','CHAR(1)',null,'I')
+;
+
