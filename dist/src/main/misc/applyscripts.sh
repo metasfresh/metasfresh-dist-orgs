6f72e7b38682db4451f95bef70b43f4e321a1bd8
metasnw <nw@metasfresh.com>
2017-03-07 14:24:19 +0100

    adding fix to support stdin/out

--- dist/src/main/misc/applyscripts.sh
+++ dist/src/main/misc/applyscripts.sh
@@ -1,7 +1,14 @@
+#!/bin/bash
 # git clone https://github.com/metasfresh/metasfresh-dist-orgs.git
 # wenn schon da: git pull
-SCRIPTS_DIR=/home/metasfresh/metasfresh-dist-orgs/base/src/main/sql/postgresql/system/90-de.metas.vertical.orgs
+
+SCRIPTS_DIR=/tmp/metasfresh-dist-orgs/base/src/main/sql/postgresql/system/90-de.metas.vertical.orgs
 DATABASE_NAME=metasfresh
+set -e
 for file in $SCRIPTS_DIR/*.sql
-    do docker exec -it metasfreshdocker_db_1 psql -U metasfresh $DATABASE_NAME -f $file
+do
+  FILE_CONTENT=$(cat $file)
+  docker exec -u postgres orgsdocker_db_1 psql -d $DATABASE_NAME -c "$FILE_CONTENT"
 done
+
+exit 0
