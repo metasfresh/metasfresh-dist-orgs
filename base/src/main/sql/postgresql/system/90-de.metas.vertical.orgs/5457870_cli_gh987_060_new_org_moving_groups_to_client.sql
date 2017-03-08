df76918e8e9efe07799f59b8f7a558ba37a20ee3
metasnw <nw@metasfresh.com>
2017-03-08 09:05:41 +0100

    fix colons

--- base/src/main/sql/postgresql/system/90-de.metas.vertical.orgs/5457870_cli_gh987_060_new_org_moving_groups_to_client.sql
+++ base/src/main/sql/postgresql/system/90-de.metas.vertical.orgs/5457870_cli_gh987_060_new_org_moving_groups_to_client.sql
@@ -1,8 +1,8 @@
 update m_product
 set ad_client_id = 99
-where value != 'm01' -- keep Mitgliedschaft;
+where value != 'm01';
 update m_product_category 
 set ad_client_id = 99
-where value != 'Standard' -- keep Standard;
+where value != 'Standard' ;
 update c_bp_group 
 set ad_org_id = 0;
