342ea13fa54a7bf5f6e6cf499aed1f5374c6ed14
metasnw <nw@metasfresh.com>
2017-03-14 16:33:24 +0100

    #987 adding kategory to cover attribute pricing
    #987 vertical for orgs

---
+++ base/src/main/sql/postgresql/system/90-de.metas.vertical.orgs/5458390_sys_gh987_add_field_kategory_order_window.sql
@@ -0,0 +1,40 @@
+-- 14.03.2017 16:21
+-- URL zum Konzept
+UPDATE AD_UI_Element SET IsDisplayed='Y',Updated=TO_TIMESTAMP('2017-03-14 16:21:42','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=1000035
+;
+
+-- 14.03.2017 16:21
+-- URL zum Konzept
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=30,Updated=TO_TIMESTAMP('2017-03-14 16:21:54','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=1000035
+;
+
+-- 14.03.2017 16:21
+-- URL zum Konzept
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=40,Updated=TO_TIMESTAMP('2017-03-14 16:21:54','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=1000037
+;
+
+-- 14.03.2017 16:21
+-- URL zum Konzept
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=50,Updated=TO_TIMESTAMP('2017-03-14 16:21:54','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=1000040
+;
+
+-- 14.03.2017 16:21
+-- URL zum Konzept
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=60,Updated=TO_TIMESTAMP('2017-03-14 16:21:54','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=1000043
+;
+
+-- 14.03.2017 16:21
+-- URL zum Konzept
+UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=70,Updated=TO_TIMESTAMP('2017-03-14 16:21:54','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=1000045
+;
+
+-- 14.03.2017 16:22
+-- URL zum Konzept
+UPDATE AD_Field SET Name='Kategorie',Updated=TO_TIMESTAMP('2017-03-14 16:22:38','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=6570
+;
+
+-- 14.03.2017 16:22
+-- URL zum Konzept
+UPDATE AD_Field_Trl SET IsTranslated='N' WHERE AD_Field_ID=6570
+;
+
