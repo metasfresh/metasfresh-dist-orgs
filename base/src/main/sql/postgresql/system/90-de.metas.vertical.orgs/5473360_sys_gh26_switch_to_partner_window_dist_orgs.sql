224ffe80049684fc65cc8a81d8f1e65e9c649291
metasnw <norbert.wessel@metasfresh.com>
2017-10-02 14:22:21 +0200

    #26 Switch to separate business partner window
    #26 changing ad_menu node to new window

---
+++ base/src/main/sql/postgresql/system/90-de.metas.vertical.orgs/5473360_sys_gh26_switch_to_partner_window_dist_orgs.sql
@@ -0,0 +1,4 @@
+-- 2017-10-02T14:12:13.061
+-- URL zum Konzept
+UPDATE AD_Menu SET ad_window_id = 540366,Updated=TO_TIMESTAMP('2017-10-02 14:12:13','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Menu_ID=110
+;
