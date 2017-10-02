6597634c94ee806ac274a038ec96e561f9c7a1ec
metasnw <norbert.wessel@metasfresh.com>
2017-10-02 14:34:21 +0200

    #26 set contact isdefault
    #26

---
+++ base/src/main/sql/postgresql/system/90-de.metas.vertical.orgs/5473360_sys_gh26_switch_to_partner_window_contact_default_value.sql
@@ -0,0 +1,4 @@
+-- 2017-10-02T14:24:50.163
+-- URL zum Konzept
+UPDATE AD_Field SET DefaultValue='Y',Updated=TO_TIMESTAMP('2017-10-02 14:24:50','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=560128
+;
