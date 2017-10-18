3646881fa1d840640cb1cca98d26d06ba194749d
metasnw <nw@metasfresh.com>
2017-10-18 08:38:16 +0200

    #55 reducing scripts to system changes
    #55

--- base/src/main/sql/postgresql/system/90-de.metas.vertical.orgs/5473360_sys_gh55_minor_change_window_partner_dist.sql
+++ base/src/main/sql/postgresql/system/90-de.metas.vertical.orgs/5473360_sys_gh55_minor_change_window_partner_dist.sql
@@ -7,3 +7,8 @@ UPDATE AD_UI_Element SET IsDisplayedGrid='N', SeqNoGrid=0,Updated=TO_TIMESTAMP('
 -- URL zum Konzept
 UPDATE AD_UI_Element SET IsDisplayedGrid='Y', SeqNoGrid=100,Updated=TO_TIMESTAMP('2017-10-17 15:46:14','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_UI_Element_ID=549095
 ;
+
+-- 2017-10-17T17:03:02.438
+-- URL zum Konzept
+UPDATE AD_Tab SET AD_Column_ID=547296,Updated=TO_TIMESTAMP('2017-10-17 17:03:02','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=540876
+;
