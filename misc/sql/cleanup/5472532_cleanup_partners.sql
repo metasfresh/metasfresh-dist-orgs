82996f7e2686f7a289bac369d426345d19107240
metasnw <nw@metasfresh.com>
2017-12-05 21:48:51 +0100

    updating cleanup scripts

---
+++ misc/sql/cleanup/5472532_cleanup_partners.sql
@@ -0,0 +1,3 @@
+delete from c_bpartner_location where c_bpartner_id in (2156425,2156423);
+delete from c_bpartner where c_bpartner_id in (2156425,2156423);
+delete from ad_user where c_bpartner_id in (2156425,2156423);
