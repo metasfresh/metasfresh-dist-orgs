9aa2522477379de95390747d8d6bae350042df4b
metasnw <nw@metasfresh.com>
2017-12-14 22:40:20 +0100

    adding default sys config for invoice rule immediately

---
+++ misc/sql/configuration/5480671_sys_invoice_rule_immediately.sql
@@ -0,0 +1,13 @@
+-- 2017-12-14T16:12:49.347
+-- URL zum Konzept
+UPDATE AD_Field SET DefaultValue='I',Updated=TO_TIMESTAMP('2017-12-14 16:12:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=549754
+;
+-- 2017-12-14T22:31:51.135
+-- URL zum Konzept
+UPDATE AD_Column SET DefaultValue='I',Updated=TO_TIMESTAMP('2017-12-14 22:31:51','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=545847
+;
+-- 2017-12-14T22:30:44.896
+-- URL zum Konzept
+INSERT INTO t_alter_column values('c_invoice_candidate','InvoiceRule_Override','CHAR(1)',null,'I')
+;
+
