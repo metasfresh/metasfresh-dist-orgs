ea6ba3ca16895ea13c961fd375bdfb1ff30ffa86
metasnw <nw@metasfresh.com>
2018-01-26 08:26:48 +0100

    setting window management view defaults

---
+++ misc/sql/configuration/Window_Table_Columns_Grid_Defaults.sql
@@ -0,0 +1,249 @@
+-- 2018-01-10T20:27:49.885
+-- URL zum Konzept
+UPDATE AD_Field SET ColumnDisplayLength=65, IsDisplayedGrid='N', SeqNoGrid=0,Updated=TO_TIMESTAMP('2018-01-10 20:27:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=255
+;
+
+-- 2018-01-10T20:27:49.891
+-- URL zum Konzept
+UPDATE AD_Field SET ColumnDisplayLength=56, IsDisplayedGrid='N', SeqNoGrid=0,Updated=TO_TIMESTAMP('2018-01-10 20:27:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=1984
+;
+
+-- 2018-01-10T20:27:49.895
+-- URL zum Konzept
+UPDATE AD_Field SET ColumnDisplayLength=250, IsDisplayedGrid='N', SeqNoGrid=0,Updated=TO_TIMESTAMP('2018-01-10 20:27:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=157
+;
+
+-- 2018-01-10T20:27:49.900
+-- URL zum Konzept
+UPDATE AD_Field SET ColumnDisplayLength=152, IsDisplayedGrid='Y', SeqNoGrid=10,Updated=TO_TIMESTAMP('2018-01-10 20:27:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=156
+;
+
+-- 2018-01-10T20:27:49.908
+-- URL zum Konzept
+UPDATE AD_Field SET ColumnDisplayLength=250, IsDisplayedGrid='N', SeqNoGrid=0,Updated=TO_TIMESTAMP('2018-01-10 20:27:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=11264
+;
+
+-- 2018-01-10T20:27:49.912
+-- URL zum Konzept
+UPDATE AD_Field SET ColumnDisplayLength=152, IsDisplayedGrid='N', SeqNoGrid=0,Updated=TO_TIMESTAMP('2018-01-10 20:27:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=1345
+;
+
+-- 2018-01-10T20:27:49.916
+-- URL zum Konzept
+UPDATE AD_Field SET ColumnDisplayLength=83, IsDisplayedGrid='N', SeqNoGrid=0,Updated=TO_TIMESTAMP('2018-01-10 20:27:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=61142
+;
+
+-- 2018-01-10T20:27:49.919
+-- URL zum Konzept
+UPDATE AD_Field SET ColumnDisplayLength=199, IsDisplayedGrid='Y', SeqNoGrid=20,Updated=TO_TIMESTAMP('2018-01-10 20:27:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=152
+;
+
+-- 2018-01-10T20:27:49.921
+-- URL zum Konzept
+UPDATE AD_Field SET ColumnDisplayLength=250, IsDisplayedGrid='N', SeqNoGrid=0,Updated=TO_TIMESTAMP('2018-01-10 20:27:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=153
+;
+
+-- 2018-01-10T20:27:49.923
+-- URL zum Konzept
+UPDATE AD_Field SET ColumnDisplayLength=250, IsDisplayedGrid='N', SeqNoGrid=0,Updated=TO_TIMESTAMP('2018-01-10 20:27:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=154
+;
+
+-- 2018-01-10T20:27:49.926
+-- URL zum Konzept
+UPDATE AD_Field SET ColumnDisplayLength=44, IsDisplayedGrid='Y', SeqNoGrid=40,Updated=TO_TIMESTAMP('2018-01-10 20:27:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=250
+;
+
+-- 2018-01-10T20:27:49.928
+-- URL zum Konzept
+UPDATE AD_Field SET ColumnDisplayLength=56, IsDisplayedGrid='N', SeqNoGrid=0,Updated=TO_TIMESTAMP('2018-01-10 20:27:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=155
+;
+
+-- 2018-01-10T20:27:49.929
+-- URL zum Konzept
+UPDATE AD_Field SET ColumnDisplayLength=49, IsDisplayedGrid='N', SeqNoGrid=0,Updated=TO_TIMESTAMP('2018-01-10 20:27:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=158
+;
+
+-- 2018-01-10T20:27:49.931
+-- URL zum Konzept
+UPDATE AD_Field SET ColumnDisplayLength=95, IsDisplayedGrid='N', SeqNoGrid=0,Updated=TO_TIMESTAMP('2018-01-10 20:27:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=542913
+;
+
+-- 2018-01-10T20:27:49.932
+-- URL zum Konzept
+UPDATE AD_Field SET ColumnDisplayLength=80, IsDisplayedGrid='N', SeqNoGrid=0,Updated=TO_TIMESTAMP('2018-01-10 20:27:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=170
+;
+
+-- 2018-01-10T20:27:49.934
+-- URL zum Konzept
+UPDATE AD_Field SET ColumnDisplayLength=145, IsDisplayedGrid='N', SeqNoGrid=0,Updated=TO_TIMESTAMP('2018-01-10 20:27:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=172
+;
+
+-- 2018-01-10T20:27:49.935
+-- URL zum Konzept
+UPDATE AD_Field SET ColumnDisplayLength=87, IsDisplayedGrid='N', SeqNoGrid=0,Updated=TO_TIMESTAMP('2018-01-10 20:27:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=310
+;
+
+-- 2018-01-10T20:27:49.937
+-- URL zum Konzept
+UPDATE AD_Field SET ColumnDisplayLength=93, IsDisplayedGrid='N', SeqNoGrid=0,Updated=TO_TIMESTAMP('2018-01-10 20:27:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=56279
+;
+
+-- 2018-01-10T20:27:49.938
+-- URL zum Konzept
+UPDATE AD_Field SET ColumnDisplayLength=96, IsDisplayedGrid='N', SeqNoGrid=0,Updated=TO_TIMESTAMP('2018-01-10 20:27:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=56374
+;
+
+-- 2018-01-10T20:27:49.940
+-- URL zum Konzept
+UPDATE AD_Field SET ColumnDisplayLength=147, IsDisplayedGrid='N', SeqNoGrid=0,Updated=TO_TIMESTAMP('2018-01-10 20:27:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=5122
+;
+
+-- 2018-01-10T20:27:49.941
+-- URL zum Konzept
+UPDATE AD_Field SET ColumnDisplayLength=56, IsDisplayedGrid='N', SeqNoGrid=0,Updated=TO_TIMESTAMP('2018-01-10 20:27:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=2526
+;
+
+-- 2018-01-10T20:27:49.943
+-- URL zum Konzept
+UPDATE AD_Field SET ColumnDisplayLength=113, IsDisplayedGrid='N', SeqNoGrid=0,Updated=TO_TIMESTAMP('2018-01-10 20:27:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=171
+;
+
+-- 2018-01-10T20:27:49.944
+-- URL zum Konzept
+UPDATE AD_Field SET ColumnDisplayLength=110, IsDisplayedGrid='N', SeqNoGrid=0,Updated=TO_TIMESTAMP('2018-01-10 20:27:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=54403
+;
+
+-- 2018-01-10T20:27:49.945
+-- URL zum Konzept
+UPDATE AD_Field SET ColumnDisplayLength=69, IsDisplayedGrid='N', SeqNoGrid=0,Updated=TO_TIMESTAMP('2018-01-10 20:27:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=2574
+;
+
+-- 2018-01-10T20:27:49.947
+-- URL zum Konzept
+UPDATE AD_Field SET ColumnDisplayLength=70, IsDisplayedGrid='N', SeqNoGrid=0,Updated=TO_TIMESTAMP('2018-01-10 20:27:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=2573
+;
+
+-- 2018-01-10T20:27:49.948
+-- URL zum Konzept
+UPDATE AD_Field SET ColumnDisplayLength=121, IsDisplayedGrid='N', SeqNoGrid=0,Updated=TO_TIMESTAMP('2018-01-10 20:27:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=160
+;
+
+-- 2018-01-10T20:27:49.949
+-- URL zum Konzept
+UPDATE AD_Field SET ColumnDisplayLength=102, IsDisplayedGrid='Y', SeqNoGrid=50,Updated=TO_TIMESTAMP('2018-01-10 20:27:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=161
+;
+
+-- 2018-01-10T20:27:49.950
+-- URL zum Konzept
+UPDATE AD_Field SET ColumnDisplayLength=117, IsDisplayedGrid='Y', SeqNoGrid=60,Updated=TO_TIMESTAMP('2018-01-10 20:27:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=162
+;
+
+-- 2018-01-10T20:27:49.951
+-- URL zum Konzept
+UPDATE AD_Field SET ColumnDisplayLength=90, IsDisplayedGrid='Y', SeqNoGrid=70,Updated=TO_TIMESTAMP('2018-01-10 20:27:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=166
+;
+
+-- 2018-01-10T20:27:49.952
+-- URL zum Konzept
+UPDATE AD_Field SET ColumnDisplayLength=77, IsDisplayedGrid='Y', SeqNoGrid=80,Updated=TO_TIMESTAMP('2018-01-10 20:27:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=2370
+;
+
+-- 2018-01-10T20:27:49.952
+-- URL zum Konzept
+UPDATE AD_Field SET ColumnDisplayLength=128, IsDisplayedGrid='N', SeqNoGrid=0,Updated=TO_TIMESTAMP('2018-01-10 20:27:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=169
+;
+
+-- 2018-01-10T20:27:49.953
+-- URL zum Konzept
+UPDATE AD_Field SET ColumnDisplayLength=117, IsDisplayedGrid='Y', SeqNoGrid=90,Updated=TO_TIMESTAMP('2018-01-10 20:27:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=10128
+;
+
+-- 2018-01-10T20:27:49.954
+-- URL zum Konzept
+UPDATE AD_Field SET ColumnDisplayLength=103, IsDisplayedGrid='N', SeqNoGrid=0,Updated=TO_TIMESTAMP('2018-01-10 20:27:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=4941
+;
+
+-- 2018-01-10T20:27:49.955
+-- URL zum Konzept
+UPDATE AD_Field SET ColumnDisplayLength=107, IsDisplayedGrid='N', SeqNoGrid=0,Updated=TO_TIMESTAMP('2018-01-10 20:27:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=50188
+;
+
+-- 2018-01-10T20:27:49.956
+-- URL zum Konzept
+UPDATE AD_Field SET ColumnDisplayLength=64, IsDisplayedGrid='Y', SeqNoGrid=100,Updated=TO_TIMESTAMP('2018-01-10 20:27:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=168
+;
+
+-- 2018-01-10T20:27:49.957
+-- URL zum Konzept
+UPDATE AD_Field SET ColumnDisplayLength=80, IsDisplayedGrid='N', SeqNoGrid=0,Updated=TO_TIMESTAMP('2018-01-10 20:27:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=159
+;
+
+-- 2018-01-10T20:27:49.958
+-- URL zum Konzept
+UPDATE AD_Field SET ColumnDisplayLength=66, IsDisplayedGrid='N', SeqNoGrid=0,Updated=TO_TIMESTAMP('2018-01-10 20:27:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=167
+;
+
+-- 2018-01-10T20:27:49.959
+-- URL zum Konzept
+UPDATE AD_Field SET ColumnDisplayLength=164, IsDisplayedGrid='N', SeqNoGrid=0,Updated=TO_TIMESTAMP('2018-01-10 20:27:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=5121
+;
+
+-- 2018-01-10T20:27:49.960
+-- URL zum Konzept
+UPDATE AD_Field SET ColumnDisplayLength=94, IsDisplayedGrid='Y', SeqNoGrid=110,Updated=TO_TIMESTAMP('2018-01-10 20:27:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=56317
+;
+
+-- 2018-01-10T20:27:49.961
+-- URL zum Konzept
+UPDATE AD_Field SET ColumnDisplayLength=151, IsDisplayedGrid='N', SeqNoGrid=0,Updated=TO_TIMESTAMP('2018-01-10 20:27:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=549455
+;
+
+-- 2018-01-10T20:27:49.962
+-- URL zum Konzept
+UPDATE AD_Field SET ColumnDisplayLength=53, IsDisplayedGrid='N', SeqNoGrid=0,Updated=TO_TIMESTAMP('2018-01-10 20:27:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=151
+;
+
+-- 2018-01-10T20:27:49.963
+-- URL zum Konzept
+UPDATE AD_Field SET ColumnDisplayLength=70, IsDisplayedGrid='N', SeqNoGrid=0,Updated=TO_TIMESTAMP('2018-01-10 20:27:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=547483
+;
+
+-- 2018-01-10T20:27:49.964
+-- URL zum Konzept
+UPDATE AD_Field SET ColumnDisplayLength=174, IsDisplayedGrid='N', SeqNoGrid=0,Updated=TO_TIMESTAMP('2018-01-10 20:27:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=549348
+;
+
+-- 2018-01-10T20:27:49.965
+-- URL zum Konzept
+UPDATE AD_Field SET ColumnDisplayLength=99, IsDisplayedGrid='Y', SeqNoGrid=120,Updated=TO_TIMESTAMP('2018-01-10 20:27:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=551629
+;
+
+-- 2018-01-10T20:27:49.966
+-- URL zum Konzept
+UPDATE AD_Field SET ColumnDisplayLength=200, IsDisplayedGrid='Y', SeqNoGrid=130,Updated=TO_TIMESTAMP('2018-01-10 20:27:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=70266
+;
+
+-- 2018-01-10T20:27:49.967
+-- URL zum Konzept
+UPDATE AD_Field SET ColumnDisplayLength=64, IsDisplayedGrid='Y', SeqNoGrid=140,Updated=TO_TIMESTAMP('2018-01-10 20:27:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=72812
+;
+
+-- 2018-01-10T20:27:49.968
+-- URL zum Konzept
+UPDATE AD_Field SET ColumnDisplayLength=112, IsDisplayedGrid='Y', SeqNoGrid=150,Updated=TO_TIMESTAMP('2018-01-10 20:27:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=555402
+;
+
+-- 2018-01-10T20:27:49.969
+-- URL zum Konzept
+UPDATE AD_Field SET ColumnDisplayLength=84, IsDisplayedGrid='Y', SeqNoGrid=160,Updated=TO_TIMESTAMP('2018-01-10 20:27:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=555825
+;
+
+-- 2018-01-10T20:27:49.970
+-- URL zum Konzept
+UPDATE AD_Field SET ColumnDisplayLength=121, IsDisplayedGrid='Y', SeqNoGrid=170,Updated=TO_TIMESTAMP('2018-01-10 20:27:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=557471
+;
+
+-- 2018-01-10T20:27:49.971
+-- URL zum Konzept
+UPDATE AD_Field SET ColumnDisplayLength=74, IsDisplayedGrid='Y', SeqNoGrid=30,Updated=TO_TIMESTAMP('2018-01-10 20:27:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=4940
+;
