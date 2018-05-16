e4f1064941d95bf3b313eb54ff9014897d2d101e
metasnw <nw@metasfresh.com>
2018-05-16 22:07:56 +0200

    disable dashboard

---
+++ misc/sql/configuration/5487750_sys_disable_dashboard.sql
@@ -0,0 +1,45 @@
+-- 2018-03-08T09:23:36.153
+-- project window
+UPDATE WEBUI_DashboardItem SET IsActive='N',Updated=TO_TIMESTAMP('2018-03-08 09:23:36','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE WEBUI_DashboardItem_ID=1000015
+;
+
+-- 2018-03-08T09:23:36.599
+-- project window
+UPDATE WEBUI_DashboardItem SET IsActive='N',Updated=TO_TIMESTAMP('2018-03-08 09:23:36','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE WEBUI_DashboardItem_ID=1000016
+;
+
+-- 2018-03-08T09:23:37.055
+-- project window
+UPDATE WEBUI_DashboardItem SET IsActive='N',Updated=TO_TIMESTAMP('2018-03-08 09:23:37','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE WEBUI_DashboardItem_ID=1000021
+;
+
+-- 2018-03-08T09:23:37.502
+-- project window
+UPDATE WEBUI_DashboardItem SET IsActive='N',Updated=TO_TIMESTAMP('2018-03-08 09:23:37','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE WEBUI_DashboardItem_ID=1000022
+;
+
+-- 2018-03-08T09:23:37.980
+-- project window
+UPDATE WEBUI_DashboardItem SET IsActive='N',Updated=TO_TIMESTAMP('2018-03-08 09:23:37','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE WEBUI_DashboardItem_ID=1000023
+;
+
+-- 2018-03-08T09:23:38.428
+-- project window
+UPDATE WEBUI_DashboardItem SET IsActive='N',Updated=TO_TIMESTAMP('2018-03-08 09:23:38','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE WEBUI_DashboardItem_ID=1000017
+;
+
+-- 2018-03-08T09:23:38.904
+-- project window
+UPDATE WEBUI_DashboardItem SET IsActive='N',Updated=TO_TIMESTAMP('2018-03-08 09:23:38','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE WEBUI_DashboardItem_ID=1000018
+;
+
+-- 2018-03-08T09:23:39.414
+-- project window
+UPDATE WEBUI_DashboardItem SET IsActive='N',Updated=TO_TIMESTAMP('2018-03-08 09:23:39','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE WEBUI_DashboardItem_ID=1000019
+;
+
+-- 2018-03-08T09:23:40.045
+-- project window
+UPDATE WEBUI_DashboardItem SET IsActive='N',Updated=TO_TIMESTAMP('2018-03-08 09:23:40','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE WEBUI_DashboardItem_ID=1000020
+;
+
