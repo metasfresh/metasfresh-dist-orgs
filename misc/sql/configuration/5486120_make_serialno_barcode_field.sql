4ec36b3f3aa25c959d587dab7b8dafa8f207c5a7
metasnw <nw@metasfresh.com>
2019-04-13 17:00:02 +0200

    more sample scripts

---
+++ misc/sql/configuration/5486120_make_serialno_barcode_field.sql
@@ -0,0 +1,5 @@
+-- 2018-02-21T07:34:36.720
+-- URL zum Konzept
+INSERT INTO DIM_Dimension_Spec_Attribute (AD_Client_ID,AD_Org_ID,AttributeValueType,Created,CreatedBy,DIM_Dimension_Spec_Attribute_ID,DIM_Dimension_Spec_ID,IsActive,IsIncludeAllAttributeValues,IsValueAggregate,M_Attribute_ID,Updated,UpdatedBy) VALUES (1000000,1000000,'S',TO_TIMESTAMP('2018-02-21 07:34:31','YYYY-MM-DD HH24:MI:SS'),100,540022,540007,'Y','Y','N',540034,TO_TIMESTAMP('2018-02-21 07:34:31','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
---
+++ misc/sql/configuration/5499340_sys_product_value_writable.sql
@@ -0,0 +1,5 @@
+-- 2018-08-15T12:03:32.284
+-- URL zum Konzept
+UPDATE AD_Field SET IsReadOnly='N',Updated=TO_TIMESTAMP('2018-08-15 12:03:32','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=1033
+;
+
