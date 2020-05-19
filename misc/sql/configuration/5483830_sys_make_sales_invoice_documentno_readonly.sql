c0ab9680398f89dfa94aa988892edc670c32152a
metasnw <nw@metasfresh.com>
2020-05-19 17:37:32 +0200

    adding misc config scripts

---
+++ misc/sql/configuration/5483830_sys_make_sales_invoice_documentno_readonly.sql
@@ -0,0 +1,4 @@
+-- sales invoice window documentno
+UPDATE AD_Field SET IsReadOnly='Y',Updated=TO_TIMESTAMP('2018-01-24 23:12:53','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=2779
+;
+
---
+++ misc/sql/configuration/5483830_sys_make_sales_order_documentno_readonly.sql
@@ -0,0 +1,3 @@
+-- sales order window documentno
+UPDATE AD_Field SET IsReadOnly='Y',Updated=TO_TIMESTAMP('2018-01-24 23:12:26','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=1081
+;
